a193c6e0a0b339d1284039603a395c0d7a90a838
martisya <martisyahalid@gmail.com>
2019-12-13 16:00:00 +0800

    voucher update

--- App/Checkout/UsedVoucher.js
+++ App/Checkout/UsedVoucher.js
@@ -142,7 +142,7 @@ const styles = StyleSheet.create({
 	descriptionText: {
 		color: "rgb(124, 124, 124)",
 		fontFamily: NON_TITLE_FONT,
-		fontSize: 12 * fontAlpha,
+		fontSize: 14 * fontAlpha,
 		fontStyle: "normal",
 		fontWeight: "normal",
 		textAlign: "left",
