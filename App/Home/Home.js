944dad3ed9abb54a9d36a75e0a98893a18cd3a4d
Tony Voon <vsching85@gmail.com>
2019-12-15 18:05:22 +0800

    fix checkout

--- App/Home/Home.js
+++ App/Home/Home.js
@@ -621,7 +621,7 @@ export default class Home extends React.Component {
 
 	getCartHeight(){
 		const { cart, promotions } = this.props
-		
+		const {product_view_height} = this.state
 		var headerHeight = 31 * alpha
 		var height = (cart.length * 71) * alpha + (promotions.length * 71) * alpha
 		
@@ -629,11 +629,13 @@ export default class Home extends React.Component {
 		var content = height + headerHeight
 
 		const bottomHeight = 45*alpha + 60 *alpha + 35*alpha + 40*alpha
-		var finalheight = windowHeight  - content - bottomHeight
+		var finalheight = product_view_height  - content
 
 		if (finalheight < height_cap) {
 			finalheight = height_cap
 		}
+
+		console.log(`height ${product_view_height}`)
 
 		return finalheight
 	}
@@ -675,9 +677,12 @@ export default class Home extends React.Component {
 	}
 
 	measureView(event) {
-		this.setState({
-			product_view_height: event.nativeEvent.layout.height
-		})
+		if (this.state.product_view_height == 0){
+			this.setState({
+				product_view_height: event.nativeEvent.layout.height
+			})
+		}
+		
 	}
 
 	renderPopOutCartFlatListCell = ({ item, index }) => {
