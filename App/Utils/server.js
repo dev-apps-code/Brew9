4710a0f907497a650adc0f772afeaa40c4bb4b76
martisya <martisyahalid@gmail.com>
2020-03-02 09:47:48 +0800

    test

--- App/Utils/server.js
+++ App/Utils/server.js
@@ -21,8 +21,8 @@ export const KURL_PRIVACY_POLICY = KURL_INFO + "?page=privacy&id=1";
 export const KURL_EULA = KURL_INFO + "?page=eula&id=1";
 export const KURL_MEMBERSHIP_INFO = KURL_INFO + '/membership_info';
 export const KCURRENT_API_VERSION_HEADER = "application/dc.v4 gzip";
-export const APPBUILDVERSIONIOS = "10";
-export const APPBUILDVERSIONANDROID = "10";
+export const APPBUILDVERSIONIOS = "11";
+export const APPBUILDVERSIONANDROID = "11";
 
 export const KTIMEOUT = 3 * 1000;
 
