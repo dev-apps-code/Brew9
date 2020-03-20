f7a24c68fe948a085d0024335b4c84e3d1e6dbe4
Ming Hui Foo <minghui.jason@gmail.com>
2020-03-20 15:03:00 +0800

    cc fix

--- App/Checkout/Checkout.js
+++ App/Checkout/Checkout.js
@@ -744,21 +744,10 @@ export default class Checkout extends React.Component {
 	}
 
 	onCreditButtonPressed = () => {
-		const { currentMember, selectedShop } = this.props
-		var credit_card_message = "System is in process of upgrading. Please select other payment options for now."
-		if (selectedShop.response_message != undefined) {
-			credit_card_response = _.find(selectedShop.response_message, function (obj) {
-				return obj.key === "Credit Card Disabled";
-			})
-			if (credit_card_message != undefined) {
-				credit_card_message = credit_card_response.text
-			}
-		}
-		this.refs.toast.show(<View style={{ justifyContent: "center" }}><Text style={{ color: "white", textAlign: "center" }}>{credit_card_message}</Text></View>, TOAST_DURATION + 1000)
-		// this.setState({
-		// 	selected_payment: "credit_card"
-		// })
-		// this.tooglePayment()
+		this.setState({
+			selected_payment: "credit_card"
+		})
+		this.tooglePayment()
 
 	}
 	onCounterButtonPressed = () => {
