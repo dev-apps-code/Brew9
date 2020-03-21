824f722129dca276dd84086afbabdc468ccfd07a
Gene M <eugene@dreamcode.solutions>
2020-03-21 12:29:21 +0800

    Removed untracked files

--- ios/Pods/Headers/Public/react-native-kochava-tracker/KochavaCore.h
+++
@@ -1,1 +0,0 @@
-../../../../../node_modules/react-native-kochava-tracker/ios/KochavaSDK/libKochavaTrackeriOS/libKochavaCoreiOS/KochavaCore.h
--- ios/Pods/Target Support Files/react-native-kochava-tracker/react-native-kochava-tracker-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_react_native_kochava_tracker : NSObject
-@end
-@implementation PodsDummy_react_native_kochava_tracker
-@end
