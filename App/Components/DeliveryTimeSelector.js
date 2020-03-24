ff97b7cf1f7d4f6298384444f0ee4bdb13c7035b
Gene M <eugene@dreamcode.solutions>
2020-03-24 11:23:56 +0800

    Refactor code

--- App/Components/DeliveryTimeSelector.js
+++ App/Components/DeliveryTimeSelector.js
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
-import { Animated, Image, Text, TouchableOpacity, View } from 'react-native';
+import {
+  Animated,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+  StyleSheet
+} from 'react-native';
 import ScrollPicker from 'rn-scrollable-picker';
 import { alpha, windowWidth } from '../Common/size';
 import {
   NON_TITLE_FONT,
   LIGHT_GREY,
-  DEFAULT_GREY_BACKGROUND
+  DEFAULT_GREY_BACKGROUND,
+  LIGHT_BLUE,
+  PRIMARY_COLOR
 } from '../Common/common_style';
+import { month } from '../Utils/date';
 
 const closeButtonImage = require('./../../assets/images/x-3.png');
 
@@ -18,8 +28,8 @@ const DelieryTimeSelector = ({
   props
 }) => {
   let { minute_range, hour_range } = state;
-  const [isToday, setIsToday] = useState(false);
-  let today= new Date();
+  const [isToday, setIsToday] = useState(true);
+  let today = new Date();
   let tomorrow = new Date();
 
   tomorrow.setDate(tomorrow.getDate() + 1);
@@ -46,33 +56,23 @@ const DelieryTimeSelector = ({
             flexDirection: 'row'
           }}
         >
-          <View
-            style={{
-              width: windowWidth / 2 - 40,
-              height: 80 * alpha,
-              backgroundColor: 'white',
-              margin: 20,
-              marginRight: 10,
-              marginLeft: 30
-            }}
-          >
-            <Text>Test</Text>
-          </View>
-          <View
-            style={{
-              width: windowWidth / 2 - 40,
-              height: 80 * alpha,
-              backgroundColor: 'white',
-              margin: 20,
-              marginLeft: 10,
-              marginRight: 30
-            }}
-          >
-            <Text>Test</Text>
-          </View>
+          <DayCard
+            cardStyle={componentStyle.todayCard}
+            isActive={isToday}
+            dayText="Today"
+            dateText={`${month(today.getMonth())} ${today.getDate()}`}
+            press={() => setIsToday(true)}
+          />
+          <DayCard
+            cardStyle={componentStyle.tomorrowCard}
+            isActive={!isToday}
+            dayText="Tomorrow"
+            dateText={`${month(tomorrow.getMonth())} ${tomorrow.getDate()}`}
+            press={() => setIsToday(false)}
+          />
         </View>
-        {/* <View style={styles.menuRowLine2View} /> */}
-        <View style={[styles.popOutTimePickerView, { height: 100 * alpha }]}>
+        <View style={styles.menuRowLine2View} />
+        <View style={[styles.popOutTimePickerView, { height: 150 * alpha }]}>
           <View style={styles.timepickerTopBar} />
           <ScrollPicker
             ref={(sphour) => {
@@ -152,18 +152,81 @@ const DelieryTimeSelector = ({
         </View>
         <View style={styles.timepickerBottomBar} />
 
-        <View style={{ paddingVertical: 20, height: 10 * alpha }}>
-          {/* <Text>Hello</Text> */}
+        {/* <View style={{ paddingVertical: 20, height: 10 * alpha }}>
           <TouchableOpacity
             onPress={() => this.onConfirmTimePicker()}
             style={{ justifyContent: 'center' }}
           >
             <Text style={styles.pickupConfirmButtonText}>Confirm</Text>
           </TouchableOpacity>
-        </View>
+        </View> */}
       </View>
     </Animated.View>
   );
 };
+
+const DayCard = ({
+  cardStyle,
+  textStyle,
+  dayText,
+  dateText,
+  isActive,
+  press
+}) => {
+  return (
+    <TouchableOpacity
+      style={[
+        componentStyle.card,
+        cardStyle,
+        {
+          backgroundColor: isActive ? PRIMARY_COLOR : '#fff'
+        }
+      ]}
+      onPress={() => press()}
+    >
+      <Text
+        style={[
+          componentStyle.cardTextStyle,
+          {
+            color: isActive ? '#fff' : '#333'
+          }
+        ]}
+      >
+        {dayText}
+      </Text>
+      <Text
+        style={[
+          componentStyle.cardTextStyle,
+          {
+            color: isActive ? '#fff' : '#333'
+          }
+        ]}
+      >
+        {dateText}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const componentStyle = StyleSheet.create({
+  card: {
+    borderRadius: 10,
+    height: 80 * alpha,
+    justifyContent: 'center',
+    margin: 20,
+    width: windowWidth / 2 - 40
+  },
+  todayCard: {
+    marginLeft: 30,
+    marginRight: 10
+  },
+  tomorrowCard: {
+    marginLeft: 10,
+    marginRight: 30
+  },
+  cardTextStyle: {
+    textAlign: 'center'
+  }
+});
 
 export default DelieryTimeSelector;
