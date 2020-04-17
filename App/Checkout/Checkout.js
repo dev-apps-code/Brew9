76cae329879b88388f07f08883a016910df32029
dustinramirez <dramirez.jsba@gmail.com>
2020-04-17 15:37:41 +0800

    test

--- App/Checkout/Checkout.js
+++ App/Checkout/Checkout.js
@@ -2346,7 +2346,7 @@ const styles = StyleSheet.create({
   deliveryNoted: {
     color: '#ff4500',
     fontFamily: NON_TITLE_FONT,
-    fontSize: 11 * fontAlpha,
+    fontSize: 12 * fontAlpha,
     fontStyle: 'normal',
     fontWeight: 'normal',
     textAlign: 'left',
