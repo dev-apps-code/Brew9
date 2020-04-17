f771169116008aab37b4f0acd34325dce9c05e8e
martisya <martisyahalid@gmail.com>
2020-04-18 00:03:50 +0800

    fix error when handle null description_fee

--- App/Checkout/Checkout.js
+++ App/Checkout/Checkout.js
@@ -1990,7 +1990,6 @@ export default class Checkout extends React.Component {
     let non_negative_subTotal_price = parseFloat(
       Math.max(0, final_price)
     ).toFixed(2);
-
     return (
       <View style={styles.deliveryAddressView}>
         <View style={styles.voucherButton}>
@@ -2065,7 +2064,7 @@ export default class Checkout extends React.Component {
             >
               <View style={{ flex: 1 }}>
                 <Text style={styles.productNameText}>Delivery fees</Text>
-                {delivery_description && (
+                {delivery_description != null && (
                   <Text style={styles.deliveryNoted}>
                     {delivery_description}
                   </Text>
