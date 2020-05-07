b8dd6da1ec669793ccfc4d24fbacfae9a4ad5798
Eugene <eugene@dreamcode.solutions>
2020-05-07 13:55:32 +0800

    server list

---
+++ App/Constants/server_list.js
@@ -0,0 +1,17 @@
+const SERVERS = [
+  'app.brew9.co/',
+  'dev.brew9.co/',
+  'dev1.brew9.co/',
+  'dev2.brew9.co/',
+  'dev3.brew9.co/',
+  'dev4.brew9.co/',
+  'dev11.brew9.co/'
+];
+
+const DEFAULT_SERVER = 'app.brew9.co/';
+
+const PROTOCOLS = ['https://'];
+
+const DEFAULT_PROTOCOL = 'https://';
+
+export { SERVERS, DEFAULT_SERVER, PROTOCOLS, DEFAULT_PROTOCOL };
