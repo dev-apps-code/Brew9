27b2ee46a8ca1dce6f4eee2c2f3dcaf4cf938247
Eugene Maning <eugene@dreamcode.solutions>
2020-06-17 10:14:07 +0800

    after build changes

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "exp://192.168.254.104:19000";
+  public static final String DEVELOPMENT_URL = "exp://192.168.1.108:19000";
 
 }
