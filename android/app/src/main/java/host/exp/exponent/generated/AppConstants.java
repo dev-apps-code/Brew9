6bec6d76f901a8387e3e5f11cff4507fe7bd7c5d
Jason <admin@Admins-MacBook-Pro.local>
2020-07-17 19:29:52 +0800

    quickfix/feedback_subject_ios

--- android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -245,10 +245,12 @@ public class AppConstants {
       
       
       
+      
+      
       // ADD EMBEDDED RESPONSES HERE
       // START EMBEDDED RESPONSES
-      embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@tonyvoon/brew9", "assets://shell-app-manifest.json", "application/json"));
-      embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40tonyvoon%2Fbrew9%2F1.2.1%2F372c3b0bf4cff0fc3e4ad5622b4e5cff-34.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+      embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@jasonfoo/brew9", "assets://shell-app-manifest.json", "application/json"));
+      embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40jasonfoo%2Fbrew9%2F1.2.5%2Fd33e1dcf7c2d4bb642dd766acd68466c-34.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
       // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
