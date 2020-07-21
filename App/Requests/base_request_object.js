5c4f0643d08463e9cebfa1ec550a73105faf2271
Eugene Maning <eugene@dreamcode.solutions>
2020-07-22 01:20:08 +0800

    Excluse shopId in form data and format

--- App/Requests/base_request_object.js
+++ App/Requests/base_request_object.js
@@ -1,28 +1,24 @@
 class BaseRequestObject {
+  setUrlId(url_id) {
+    this.url_id = url_id;
+  }
 
-    setUrlId(url_id) {
-        this.url_id = url_id
-    }
+  setPage(page) {
+    this.page_no = page;
+  }
 
-    setPage(page) {
-        this.page_no = page
-    }
-
-    getFormData() {
-
-        let formBody = []
-        Object.entries(this).forEach(entry => {
-
-            if (entry[0] != 'url_id') {
-
-                const encodedKey = encodeURIComponent(entry[0]);
-                const encodedValue = encodeURIComponent(entry[1]);
-                formBody.push(`${encodedKey}=${encodedValue}`)
-            }
-            //use key and value here
-        });
-        formBody = formBody.join('&')
-        return formBody
-    }
+  getFormData() {
+    let formBody = [];
+    Object.entries(this).forEach((entry) => {
+      if (entry[0] != 'url_id' && entry[0] !== 'shopId') {
+        const encodedKey = encodeURIComponent(entry[0]);
+        const encodedValue = encodeURIComponent(entry[1]);
+        formBody.push(`${encodedKey}=${encodedValue}`);
+      }
+      //use key and value here
+    });
+    formBody = formBody.join('&');
+    return formBody;
+  }
 }
-export default BaseRequestObject
+export default BaseRequestObject;
