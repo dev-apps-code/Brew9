898e3a7d672f8c65cf86402f42c798b83aee79d0
dustin <dustin@dreamcode.solutions>
2020-08-10 16:52:12 +0800

    clean code

--- App/Components/ShopDetails.js
+++ App/Components/ShopDetails.js
@@ -56,12 +56,18 @@ export default class ShopDetails extends Component {
   }
 
   onPressDirection(lat, long) {
+    let latitude = lat ? parseFloat(lat) : 4.8886091;
+    let longitude = long ? parseFloat(long) : 114.8976136;
+    let location = latitude + ',' + longitude;
     const url = Platform.select({
-      ios: 'https://www.google.com/maps/dir/?api=1&origin=My+Location&destination=' + lat + ',' + long,
-      android: 'https://www.google.com/maps/dir/?api=1&destination=' + lat + ',' + long
+      ios: 'https://www.google.com/maps/dir/?api=1&origin=My+Location&destination=' + location,
+      android: 'https://www.google.com/maps/dir/?api=1&destination=' + location
     })
 
-    Linking.openURL(url); 
+
+    Linking.openURL(url);
+   
+
   }
 
   renderAvailablity = (availability) => {
