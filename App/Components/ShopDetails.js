9020c00769865def57ff1b43d00d256965b57d8b
Eugene Maning <eugene@dreamcode.solutions>
2020-08-11 16:27:47 +0800

    :lipstick: adjust gap between address and time

--- App/Components/ShopDetails.js
+++ App/Components/ShopDetails.js
@@ -18,7 +18,6 @@ import {
   DISABLED_COLOR
 } from '../Common/common_style';
 import { alpha, fontAlpha } from '../Common/size';
-import openMap from 'react-native-open-maps';
 
 @connect(({ members, shops, config }) => ({
   currentUser: members.profile,
@@ -45,11 +44,7 @@ export default class ShopDetails extends Component {
           style={styles.favoriteButton}
           {...{ disabled }}
         >
-          <Image 
-          resizeMode="contain"
-          source={likeImage}
-          />
-          
+          <Image resizeMode="contain" source={likeImage} />
         </TouchableOpacity>
       );
     }
@@ -64,14 +59,13 @@ export default class ShopDetails extends Component {
     let longitude = long ? parseFloat(long) : 114.8976136;
     let location = latitude + ',' + longitude;
     const url = Platform.select({
-      ios: 'https://www.google.com/maps/dir/?api=1&origin=My+Location&destination=' + location,
+      ios:
+        'https://www.google.com/maps/dir/?api=1&origin=My+Location&destination=' +
+        location,
       android: 'https://www.google.com/maps/dir/?api=1&destination=' + location
-    })
-
+    });
 
     Linking.openURL(url);
-   
-
   }
 
   renderAvailablity = (availability) => {
@@ -146,7 +140,7 @@ export default class ShopDetails extends Component {
               {details.short_address}
             </Text>
           </View>
-          <View style={{...styles.detailTextContainer,...{alignItems: 'center'}}}>
+          <View style={[styles.detailTextContainer, { marginTop: 0 }]}>
             <Image
               source={require('./../../assets/images/Group.png')}
               style={styles.clockImage}
@@ -217,7 +211,7 @@ const styles = StyleSheet.create({
   orderNowView: {
     flex: 1.5,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
     // backgroundColor: 'red'
   },
   detailView: {
@@ -229,13 +223,14 @@ const styles = StyleSheet.create({
     marginBottom: alpha * 3,
     marginTop: alpha * 6,
     justifyContent: 'flex-start',
+    alignItems: 'center',
     marginBottom: alpha * 5
   },
   orderButton: {
     width: '100%',
     height: alpha * 30,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
     // backgroundColor: 'blue'
   },
   favoriteButton: {
@@ -246,7 +241,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     right: 0,
     bottom: 0,
-    zIndex: 1,
+    zIndex: 1
   },
   accessButton: {
     flex: 1,
@@ -297,5 +292,5 @@ const styles = StyleSheet.create({
     width: 10 * alpha,
     height: 10 * alpha,
     marginRight: alpha * 8
-  },
+  }
 });
