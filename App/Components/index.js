a8cb70ba0bfb8f7a6dd3af4d2cd144f7c1b4fb54
Eugene <eugene@dreamcode.solutions>
2020-09-08 01:13:07 +0800

    refactor :recycle: :zap: change to memo and functional component

--- App/Components/index.js
+++ App/Components/index.js
@@ -1,3 +1,4 @@
+export * from './AnimationLoading';
 export {default as HudLoading} from './HudLoading';
 export {default as Brew9Toast} from './Brew9Toast';
 export {default as Brew9Modal} from './Brew9Modal';
