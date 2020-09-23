e94f80ea6da45b90a5dc4df4d989da6cca903896
dustin <dustin@dreamcode.solutions>
2020-09-23 21:48:01 +0800

    added conditions

--- App/Home/Home.js
+++ App/Home/Home.js
@@ -2012,39 +2012,91 @@ class Home extends React.Component {
   shouldShowFeatured(shop) {
     const {currentMember} = this.props;
     //jira
-    // this.onFeaturedPromotionPressed(shop.featured_promotion);
-    if (shop) {
-      AsyncStorage.getItem('featured', (err, result) => {
-        if (shop.featured_promotion != null) {
-          if (result == null || result != shop.featured_promotion.id) {
-            if (currentMember != null) {
-              if (
-                shop.featured_promotion.for_new_user == true &&
-                currentMember.first_time_buyer == true
-              ) {
-                AsyncStorage.setItem(
-                  'featured',
-                  JSON.stringify(shop.featured_promotion.id),
-                );
-                this.onFeaturedPromotionPressed(shop.featured_promotion);
-              } else if (shop.featured_promotion.for_new_user == false) {
-                AsyncStorage.setItem(
-                  'featured',
-                  JSON.stringify(shop.featured_promotion.id),
-                );
-                this.onFeaturedPromotionPressed(shop.featured_promotion);
-              }
-            } else {
+    if (shop?.featured_promotion && currentMember != null) {
+      const {featured_promotion} = shop;
+      console.log(featured_promotion);
+      const {always_on, id} = featured_promotion;
+
+      AsyncStorage.getItem('featuredPromotionIds', (err, result) => {
+        if (result) {
+          const featuredPromotionIds = result.split(',');
+          const wasSeen = featuredPromotionIds.includes(id.toString());
+          console.log('\n\n');
+          console.log('record');
+          console.log(featuredPromotionIds);
+          console.log('always on');
+          console.log(always_on);
+          console.log('was seen: ');
+          console.log(wasSeen);
+          console.log('id');
+          console.log(id);
+
+          if (always_on === true) {
+            console.log('show');
+            this.onFeaturedPromotionPressed(featured_promotion);
+            featuredPromotionIds.push(id);
+            AsyncStorage.setItem(
+              'featuredPromotionIds',
+              featuredPromotionIds.toString(),
+            );
+          } else if (always_on === false) {
+            if (wasSeen === true) {
+              console.log('no show');
+              return;
+            } else if (wasSeen === false) {
+              console.log('show');
+              this.onFeaturedPromotionPressed(featured_promotion);
+              featuredPromotionIds.push(id);
               AsyncStorage.setItem(
-                'featured',
-                JSON.stringify(shop.featured_promotion.id),
+                'featuredPromotionIds',
+                featuredPromotionIds.toString(),
               );
-              this.onFeaturedPromotionPressed(shop.featured_promotion);
             }
           }
+        } else {
+          this.onFeaturedPromotionPressed(featured_promotion);
+          const featuredPromotionIds = [id];
+          AsyncStorage.setItem(
+            'featuredPromotionIds',
+            featuredPromotionIds.toString(),
+          );
         }
       });
+
+      // this.onFeaturedPromotionPressed(shop.featured_promotion);
     }
+    // if (shop) {
+    //   AsyncStorage.getItem('featured', (err, result) => {
+    //     if (shop.featured_promotion != null) {
+    //       if (result == null || result != shop.featured_promotion.id) {
+    //         if (currentMember != null) {
+    //           if (
+    //             shop.featured_promotion.for_new_user == true &&
+    //             currentMember.first_time_buyer == true
+    //           ) {
+    //             AsyncStorage.setItem(
+    //               'featured',
+    //               JSON.stringify(shop.featured_promotion.id),
+    //             );
+    //             this.onFeaturedPromotionPressed(shop.featured_promotion);
+    //           } else if (shop.featured_promotion.for_new_user == false) {
+    //             AsyncStorage.setItem(
+    //               'featured',
+    //               JSON.stringify(shop.featured_promotion.id),
+    //             );
+    //             this.onFeaturedPromotionPressed(shop.featured_promotion);
+    //           }
+    //         } else {
+    //           AsyncStorage.setItem(
+    //             'featured',
+    //             JSON.stringify(shop.featured_promotion.id),
+    //           );
+    //           this.onFeaturedPromotionPressed(shop.featured_promotion);
+    //         }
+    //       }
+    //     }
+    //   });
+    // }
   }
 
   renderFeaturedPromo(shop, cart) {
