87eee311a0fe96624062296082b6f866dd26c341
Eugene <eugene@dreamcode.solutions>
2020-10-20 23:22:41 +0800

    version increment 2.0.6 build 34

--- App/Common/config.js
+++ App/Common/config.js
@@ -1,4 +1,4 @@
 export const ANALYTICS_ID = 'UA-151223427-1';
 export const DEVELOP_MODE = true;
-export const BUILD_VERSION_IOS = '33';
-export const BUILD_VERSION_ANDROID = '33';
+export const BUILD_VERSION_IOS = 34;
+export const BUILD_VERSION_ANDROID = 34;
